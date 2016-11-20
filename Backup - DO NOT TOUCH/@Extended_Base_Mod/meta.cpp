2b2ef41134330781468e25ef14f19b042487d7d1
paulgali <paulgali@live.co.uk>
2016-11-20 19:33:46 +0400

    Backup, do not alter
    Initial backup from 20.11.2016

---
+++ Backup - DO NOT TOUCH/@Extended_Base_Mod/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 647753401;
+name = "Extended_Base_Mod";
+timestamp = 5247781371853011128;
---
+++ Backup - DO NOT TOUCH/@Extended_Base_Mod/mod.cpp
@@ -0,0 +1,5 @@
+name = "Extended Base Mod";
+actionName = "EBM";
+version = "0.2.7";
+picture = "extendedbase.paa";
+author = "Freakylein";
